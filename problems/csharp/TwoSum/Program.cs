01be6e22b9e4d78362e25e344f5a29f2710e6190
andersonaires-cc <andersonairesspp@hotmail.com>
2025-03-08 23:14:19 -0300

    Solution TwoSum

---
+++ problems/csharp/TwoSum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TwoSum;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        var result = TwoSum.Solution.TwoSum(nums, target);
+        Console.WriteLine($"Resultado: [{string.Join(", ", result)}]");
+    }
+
+}

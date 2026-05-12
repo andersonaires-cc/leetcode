fd9d2109b5b17ec45975328300cc04023548a62c
andersonaires-cc <andersonairesspp@hotmail.com>
2026-05-11 22:07:37 -0300

    Solution Zig Zag conversion Stack

--- problems/java/ZigZagConversion/Main.java
+++ problems/java/ZigZagConversion/Main.java
@@ -1,5 +1,20 @@
 package problems.java.ZigZagConversion;
 
 public class Main {
-    
+     public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(
+                solution.convert("PAYPALISHIRING", 3)
+        );
+
+        System.out.println(
+                solution.convert("PAYPALISHIRING", 4)
+        );
+
+        System.out.println(
+                solution.convert("ABCD", 2)
+        );
+    }
 }
--- problems/java/ZigZagConversion/Solution.java
+++ problems/java/ZigZagConversion/Solution.java
@@ -1,9 +1,51 @@
 package problems.java.ZigZagConversion;
 
+import java.util.Stack;
+
 public class Solution {
     public String convert(String s, int numRows)
     {
-        return s;
+        if(numRows ==1 || s.length() <= numRows){
+            return s;
+        }
+        StringBuilder[] rows = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int index = 0;
+
+        while (index < s.length()) {
+      
+            for (int row = 0;
+                 row < numRows && index < s.length();
+                 row++) {
+
+                rows[row].append(s.charAt(index++));
+
+                if (row != 0 && row != numRows - 1) {
+                    stack.push(row);
+                }
+            }
+
+            while (!stack.isEmpty() && index < s.length()) {
+
+                int row = stack.pop();
+
+                rows[row].append(s.charAt(index++));
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+
+        return result.toString();
     }
     
 }
